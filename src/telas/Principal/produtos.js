4d859afe1d515c3f71acfbfb01dfc367b5bf0f28
Fábio Mori <fsmmori@gmail.com>
2022-10-17 14:00:33 -0300

    first commit

---
+++ src/telas/Principal/produtos.js
@@ -0,0 +1,33 @@
+import matriz4You from '../../imagens/matriz4You.png';
+import matrizDados from '../../imagens/matrizDados.png';
+import escolaMatriz from '../../imagens/escolaMatriz.png';
+import matrizEscolas from '../../imagens/matrizEscolas.png';
+import matrizKids from '../../imagens/matrizKids.png';
+
+export const produtos = [
+  {
+    imagem: matriz4You,
+    texto: "Bootcamp Matriz4YOU",
+    preco: 100.00
+  },
+  {
+    imagem: matrizDados,
+    texto: "Consultoria de dados",
+    preco: 600.00
+  },
+  {
+    imagem: matrizEscolas,
+    texto: "Palestra Escola Matriz",
+    preco: 500.00
+  },
+  {
+    imagem: escolaMatriz,
+    texto: "Camiseta Escola Matriz",
+    preco: 40.00
+  },
+  {
+    imagem: matrizKids,
+    texto: "eBook MatrizKIDS",
+    preco: 20.00
+  },
+]
